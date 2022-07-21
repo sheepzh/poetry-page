079eb1a3a1735a551077a5168b06f5cd7c785fb5
ZHY <returnzhy1996@outlook.com>
2022-07-21 19:46:59 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -14,7 +14,6 @@ function setTotal(totalCount, poetCount, wordCount, timestamp) {
         const hour = time.getHours()
         const minute = time.getMinutes()
         const seconds = time.getSeconds()
-        console.log(seconds)
         tsStr = `${year}-${n2d(month)}-${n2d(date)} ${n2d(hour)}:${n2d(minute)}:${n2d(seconds)} 更新`
     }
     document.getElementById(
@@ -27,66 +26,79 @@ setTotal()
 // Update count of poet
 getJson('/meta.json', (data) => setTotal(data.ac, data.pc, data.wc, data.ts))
 
-function groupByFirstLetter(data) {
-    const grouped = {}
-    data = data.sort((a, b) => (a.n === b.n ? 0 : a.n > b.n ? 1 : -1))
-    for (const item of data) {
-        let firstLetter = item.p[0]
-        if (!/[a-z]/.test(firstLetter)) {
-            firstLetter = '#'
-        }
-        let existList = grouped[firstLetter]
-        !existList && (existList = grouped[firstLetter] = [])
-        existList.push(item)
-    }
-    return grouped
+function renderLabel(key, label) {
+    const a = document.createElement('a')
+    const realLabel = label || key
+    a.innerText = realLabel
+    a.setAttribute("label-key", key)
+    a.id = `label-${key}`
+    a.classList.add("letter-label")
+    a.onclick = () => clickTab(key)
+    return a
 }
 
-const cardContainer = document.getElementById('poet-card-container')
-
-function renderCard(label, items) {
-    // table
-    const table = document.createElement('table')
-    table.classList.add('card-table')
-
+function init() {
     // label
-    const labelTr = document.createElement('tr')
-    const labelTd = document.createElement('td')
-    labelTd.classList.add('card-label')
-    labelTd.innerText = label
-    labelTr.append(labelTd)
-    table.append(labelTr)
-
-    // links
-    if (items && items.length) {
-        const linkTr = document.createElement('tr')
-        const linkTd = document.createElement('td')
-        linkTd.classList.add('card-link')
-        for (item of items) {
-            const link = document.createElement('a')
-            const name = item.n
-            link.innerText = name
-            link.href = `/poet.html?name=${name}`
-            linkTd.append(link)
-        }
-        linkTr.append(linkTd)
-        table.append(linkTr)
-    }
-
-    cardContainer.append(table)
-}
-
-function renderCards(data) {
-    const grouped = groupByFirstLetter(data)
     const charCodeOfLowerA = 'a'.charCodeAt(0)
     const charCodeOfUpperA = 'A'.charCodeAt(0)
+    const labelTd = document.createElement('td')
+    labelTd.classList.add('card-label')
+
     for (let i = 0; i < 26; i++) {
         const key = String.fromCharCode(charCodeOfLowerA + i)
         const label = String.fromCharCode(charCodeOfUpperA + i)
-        renderCard(label, grouped[key] || [])
+        labelTd.append(renderLabel(key, label))
     }
-    renderCard('#', grouped['#'] || [])
+    labelTd.append(renderLabel('#'))
+
+    const labelTr = document.createElement('tr')
+    labelTr.append(labelTd)
+    const table = document.getElementById('table-container')
+    table.append(labelTr)
+
+    // content
+    const contentTr = document.createElement('tr')
+    const contentTd = document.createElement('td')
+    contentTd.id = "content-td"
+    contentTr.append(contentTd)
+    table.append(contentTr)
+}
+
+function clickTab(key) {
+    Array.from(document.getElementsByClassName("letter-label")).forEach(ele => {
+        ele.classList.remove("active")
+        const clzList = ele.classList
+        if (ele.id === `label-${key}`) {
+            clzList.add("active")
+        } else {
+            clzList.remove("active")
+        }
+    })
+
+    const contentTd = document.getElementById('content-td')
+    contentTd.childNodes.forEach(c => c.remove())
+    contentTd.innerText = "数据查询中..."
+    getJson(`/__poet_${encodeURIComponent(key)}.json`, (items) => {
+        contentTd.innerText = ''
+        if (!items || !items.length) {
+            contentTd.innerText = '无数据'
+        } else {
+            items.forEach(item => {
+                const link = document.createElement('a')
+                link.classList.add('card-link')
+                const name = item.n
+                link.innerText = name
+                link.href = `/poet.html?name=${name}`
+                contentTd.append(link)
+            })
+        }
+    }, () => {
+        contentTd.innerText = '无数据'
+    })
 }
 
 // Render the card
-document.body.onload = () => getJson('/list.json', renderCards)
+document.body.onload = () => {
+    init()
+    clickTab("a")
+}
